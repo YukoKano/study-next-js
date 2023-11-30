b6600ae184a3ab84db96b6e0aee986deb8c24775
YukoKano <halloween_1031@hotmail.co.jp>
2023-12-01 00:34:45 +0900

    Update AppraisalEntryForm.jsx
    ちょっと整理

--- src/components/AppraisalEntryForm/AppraisalEntryForm.jsx
+++ src/components/AppraisalEntryForm/AppraisalEntryForm.jsx
@@ -1,86 +1,37 @@
 import styles from '@/styles/AppraisalEntryForm.module.css'
 import { useState } from 'react'
 
-const menu = [
-  {
-    value: 'curyy',
-    name: 'カレー'
-  },
-  {
-    value: 'stew',
-    name: 'シチュー'
-  }
-]
-const ingredients = [
-  {
-    makerName: 'curry', // まだ使ってない
-    value: 'onion',
-    name: '玉ねぎ'
-  },
-  {
-    makerName: 'curry',
-    value: 'carrot',
-    name: 'にんじん'
-  }
-]
-
-const years = [
-  {
-    value: '2022',
-    name: '2022'
-  },
-  {
-    value: '2023',
-    name: '2023'
-  },
-]
-
-const elapsedTimes = [
-  {
-    value: '5',
-    name: '5分'
-  },
-  {
-    value: '10',
-    name: '10分'
-  },
-  {
-    value: '15',
-    name: '15分'
-  },
-]
-
 export const AppraisalEntryForm = () => {
   const [displayModal, setDisplayModal] = useState(false);
   const [menuItem, setMenuItem] = useState('選択してください');
+
   const displayClassName = displayModal ? styles.displayBlock : styles.displayNone;
 
 
-  const handleOnClick = () => {
-    console.log('clicked');
+  const toggleDisplayModal = () => { //handleHogeとかの方がいいけど一旦
     setDisplayModal(!displayModal);
   }
 
-  const handleListOnClick = (e) => {
+  const setMenuName = (e) => {
     e.preventDefault()
     setMenuItem(e.currentTarget.value)
-    setDisplayModal(!displayModal);
+    toggleDisplayModal();
   }
 
   return (
     <form>
       <label>メニュー名</label>
-      <p className={styles.selectbox} onClick={handleOnClick}>{ menuItem }</p>
+      <p className={styles.selectbox} onClick={toggleDisplayModal}>{ menuItem }</p>
 
-      <div className={`${displayClassName} ${styles.modal}`}>
+      <div className={`${styles.modal} ${displayClassName}`}>
         <p>メニューを選んでね</p>
         <div>
-          <button onClick={handleListOnClick} value='カレー'>カレー</button>
-          <button onClick={handleListOnClick} value='シチュー'>シチュー</button>
-          <button onClick={handleListOnClick} value='グラタン'>グラタン</button>
+          <button onClick={setMenuName} value='カレー'>カレー</button>
+          <button onClick={setMenuName} value='シチュー'>シチュー</button>
+          <button onClick={setMenuName} value='グラタン'>グラタン</button>
         </div>
       </div>
-      <div className={`${styles.background} ${displayClassName}`} onClick={handleOnClick}></div>
+      <div className={`${styles.background} ${displayClassName}`} onClick={toggleDisplayModal}></div>
 
       <button className={styles.nextButton} onClick={(e) => { e.preventDefault() }}>次へ</button>
     </form>
