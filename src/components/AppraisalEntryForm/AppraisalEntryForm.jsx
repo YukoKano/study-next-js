fb1d88c7ca692c326d296b8d1f215e2ae8c54944
YukoKano <halloween_1031@hotmail.co.jp>
2023-12-10 21:29:17 +0900

    まとめられるのまとめる

--- src/components/AppraisalEntryForm/AppraisalEntryForm.jsx
+++ src/components/AppraisalEntryForm/AppraisalEntryForm.jsx
@@ -4,8 +4,10 @@ import { COURSES } from '@/constants/COURSES';
 import { Lists } from '../AppraisalEntryFormLists/AppraisalEntryFormLists';
 import { SelectedMenu } from '../AppraisalEntryFormSelectedMenu/AppraisalEntryFormSelectedMenu';
 
+const findCategory = (type) => COURSES.find((e) => e.value === type);
+
 const Label = ({ text, toggleDisplayModal, type}) => {
-  const category = COURSES.find((e) => e.value === type);
+  const category = findCategory(type);
   return (
     <>
       <label className={styles.label}>{category.label}</label>
@@ -15,7 +17,7 @@ const Label = ({ text, toggleDisplayModal, type}) => {
 }
 
 const Modal = ({ modal, handleClick, toggleDisplayModal, type }) => {
-  const category = COURSES.find((e) => e.value === type);
+  const category = findCategory(type);
 
   return (
     modal && (
@@ -39,7 +41,7 @@ const SelectBox = ({ text, valueModal, type, toggleDisplayModal, handleMenu }) =
 
 
 const NextButton = ({ menu, setSelectedMenu }) => {
-  const notEntered = menu.appetizer === "選択してください" ||  menu.soup === "選択してください" || menu.meatDish === "選択してください" || menu.dessert === "選択してください"
+  const notEntered = Object.values(menu).some(value => value === '選択してください'); // ここ分かってない
   const nextButtonClick = (e) => {
     e.preventDefault();
     if (notEntered) {
@@ -61,83 +63,56 @@ const NextButton = ({ menu, setSelectedMenu }) => {
 export const AppraisalEntryForm = () => {
   const [displayBackground, setDisplayBackground] = useState(false);
 
-  const [appetizerModal, setAppetizerModal] = useState(false);
-  const [soupModal, setSoupModal] = useState(false);
-  const [meatDishModal, setMeatDishModal] = useState(false);
-  const [dessertModal, setDessertModal] = useState(false);
+  const initialModals = {
+    appetizer: false,
+    soup: false,
+    meatDish: false,
+    dessert: false
+  }
+  const [modals, setModals] = useState(initialModals);
 
-  const [appetizer, setAppetizer] = useState('選択してください');
-  const [soup, setSoup] = useState('選択してください');
-  const [meatDish, setMeatDish] = useState('選択してください');
-  const [dessert, setDessert] = useState('選択してください');
+  const initialMenu = {
+    appetizer: '選択してください',
+    soup: '選択してください',
+    meatDish: '選択してください',
+    dessert: '選択してください'
+  }
+  const [menu, setMenu] = useState(initialMenu);
 
   const [selectedMenu, setSelectedMenu] = useState(false);
 
-  const menu = {
-    appetizer: appetizer,
-    soup: soup,
-    meatDish: meatDish,
-    dessert: dessert
-  }
-
   const toggleDisplayModal = (value) => {
     setDisplayBackground(!displayBackground);
-    if (value === "appetizer") {
-      setAppetizerModal(!appetizerModal);
-    } else if (value === "soup") {
-      setSoupModal(!soupModal);
-    } else if (value === "meatDish") {
-      setMeatDishModal(!meatDishModal);
-    } else if (value === "dessert") {
-      setDessertModal(!dessertModal);
-    }
+    setModals(prevModals => ({ ...prevModals, [value]: !prevModals[value] })); //ここわからん
   }
 
   const handleMenu = (type, e) => {
     e.preventDefault();
     const text = e.currentTarget.innerText;
 
-    switch (type) {
-      case "appetizer":
-        setAppetizer(text);
-        break;
-      case "soup":
-        setSoup(text);
-        break;
-      case "meatDish":
-        setMeatDish(text);
-        break;
-      case "dessert":
-        setDessert(text);
-        break;
-      default:
-        break;
-    }
-    toggleDisplayModal(type);
+    setMenu(prevMenu => ({ ...prevMenu, [type]: text  }));
+    setDisplayBackground(!displayBackground);
+    setModals(prevModals => ({ ...prevModals, [type]: false }));
   }
 
   const handleBackground = () => {
     setDisplayBackground(false);
-    setAppetizerModal(false);
-    setSoupModal(false);
-    setMeatDishModal(false);
-    setDessertModal(false);
+    setModals(initialModals);
   }
 
   return (
     <>
       <form>
-        <SelectBox type="appetizer" text={appetizer} valueModal={appetizerModal} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
-        <SelectBox type="soup" text={soup} valueModal={soupModal} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
-        <SelectBox type="meatDish" text={meatDish} valueModal={meatDishModal} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
-        <SelectBox type="dessert" text={dessert} valueModal={dessertModal} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
-
-        {displayBackground && (
-          <div className={styles.background} onClick={handleBackground}></div>
-        )}
-
+        <SelectBox type="appetizer" text={menu.appetizer} valueModal={modals.appetizer} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
+        <SelectBox type="soup" text={menu.soup} valueModal={modals.soup} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
+        <SelectBox type="meatDish" text={menu.meatDish} valueModal={modals.meatDish} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
+        <SelectBox type="dessert" text={menu.dessert} valueModal={modals.dessert} toggleDisplayModal={toggleDisplayModal} handleMenu={handleMenu} />
         <NextButton menu={menu} setSelectedMenu={setSelectedMenu} />
       </form>
+
+      {displayBackground && (
+        <div className={styles.background} onClick={handleBackground}></div>
+      )}
 
       {selectedMenu && (
         <SelectedMenu menu={menu} />
