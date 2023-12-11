1d8522670ad430e65aaeb91702a8e23b431d67e0
YukoKano <halloween_1031@hotmail.co.jp>
2023-12-11 23:58:00 +0900

    delete photos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,8 @@ import Link from "next/link"
 export const Header = () => {
   return (
     <header>
-      <Link href="/"><h1>myPortfolio</h1></Link>
+      <Link href="/"><h1>Study</h1></Link>
       <nav>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/photogallery">Photo Gallery</Link>
         <Link href="/study">Study</Link>
       </nav>
     </header>
